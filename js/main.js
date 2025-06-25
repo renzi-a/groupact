10f766c3d7f4539b1702a5ffa39e9a74bca7acb6
leyn <sevilla.lainemervinel.m@gmail.com>
2025-06-25 16:30:51 +0800

    Fixed the toggleMenu function on main.js file

--- js/main.js
+++ js/main.js
@@ -7,8 +7,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 function toggleMenu() {
-  const menu = document.getElementById("menu");
-  menu.classList.toggle("open");
     const button = document.querySelector('button');
     if (button.textContent === 'Toggle Menu') {
         button.textContent = 'Menu Toggled';
