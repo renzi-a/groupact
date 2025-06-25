dbf8a516d96f25698499c52487c2b06bdcd3be59
leyn <sevilla.lainemervinel.m@gmail.com>
2025-06-25 15:47:51 +0800

    Added a toggleMenu function on main.js

--- js/main.js
+++ js/main.js
@@ -3,4 +3,12 @@ documment.getElementById("title").innerText = "Welcome";
 
 document.getElementById("menuBtn").addEventListener("click", toggleMenu);
 
-// toggleMenu is not defined
+function toggleMenu() {
+    const button = document.querySelector('button');
+
+    if (button.textContent === 'Toggle Menu') {
+        button.textContent = 'Menu Toggled';
+    } else {
+        button.textContent = 'Toggle Menu';
+    }
+}
