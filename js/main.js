9b5f3455d00c5f42cd786d1b360f524ef6bef0d7
Shawi <taylamae.u@gmail.com>
2025-06-25 15:48:27 +0800

    Added changes in main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-// Buggy main.js
-documment.getElementById("title").innerText = "Welcome";
+document.getElementById("title").innerText = "Welcome";
 
 document.getElementById("menuBtn").addEventListener("click", toggleMenu);
 
-// toggleMenu is not defined
+function toggleMenu() {
+  const menu = document.getElementById("menu");
+  menu.classList.toggle("open");
+}
