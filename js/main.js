a1d7b2df2d322318a8fa7dfff1e90d977d58c899
leyn <sevilla.lainemervinel.m@gmail.com>
2025-06-25 15:56:57 +0800

    Changed the typo word from documment to document on main.js file

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // Buggy main.js
-documment.getElementById("title").innerText = "Welcome";
+document.getElementById("title").innerText = "Welcome";
 
 document.getElementById("menuBtn").addEventListener("click", toggleMenu);
 
